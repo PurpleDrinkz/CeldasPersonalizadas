d573e6917e8a740da7e860cb0eae821adbfb9212
Alumno <ulsa@iMacD108.local>
2018-09-18 18:48:23 -0700

    Celdas personalizadas perronas con nombres y promedios

---
+++ CeldasPersonalizadas/CeldaAlumno.swift
@@ -0,0 +1,20 @@
+//
+//  CeldaAlumno.swift
+//  CeldasPersonalizadas
+//
+//  Created by Alumno on 18/09/18.
+//  Copyright © 2018 Benjamin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CeldaAlumno : UITableViewCell
+{
+    
+    @IBOutlet weak var lblAlumno: UILabel!
+    @IBOutlet weak var lblPromedio: UILabel!
+    
+    
+}
+ 
